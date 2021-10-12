b325f2621fd158d46078a33f13698b8fcb012a27
박진영 <nazzang49@naver.com>
2021-10-13 01:00:42 +0900

    UPDATE : setting

--- code/train.py
+++ code/train.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import sys
+import torch
 
 from typing import List, Callable, NoReturn, NewType, Any
 import dataclasses
@@ -14,6 +15,7 @@ from transformers import (
     HfArgumentParser,
     TrainingArguments,
     set_seed,
+    EarlyStoppingCallback
 )
 
 from tokenizers import Tokenizer
@@ -28,6 +30,7 @@ from arguments import (
     DataTrainingArguments,
 )
 
+from wandb import wandb
 
 logger = logging.getLogger(__name__)
 
@@ -35,8 +38,6 @@ logger = logging.getLogger(__name__)
 def main():
     # 가능한 arguments 들은 ./arguments.py 나 transformer package 안의 src/transformers/training_args.py 에서 확인 가능합니다.
     # --help flag 를 실행시켜서 확인할 수 도 있습니다.
-
-    wandb.init(project="test", entity="bc-ai-it-mrc")
 
     parser = HfArgumentParser(
         (ModelArguments, DataTrainingArguments, TrainingArguments)
@@ -51,9 +52,13 @@ def main():
     print(f"model is from {model_args.model_name_or_path}")
     print(f"data is from {data_args.dataset_name}")
 
+    # keep wandb
+    wandb.init(project="T2096_dev", entity="bc-ai-it-mrc", name=f"[박진영][{model_args.model_name_or_path}][test]")
+
     # logging 설정
     logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(name)s -    %(message)s",
+        # format="%(asctime)s - %(levelname)s - %(name)s -    %(message)s",
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
         datefmt="%m/%d/%Y %H:%M:%S",
         handlers=[logging.StreamHandler(sys.stdout)],
     )
@@ -61,28 +66,21 @@ def main():
     # verbosity 설정 : Transformers logger의 정보로 사용합니다 (on main process only)
     logger.info("Training/evaluation parameters %s", training_args)
 
-    # 모델을 초기화하기 전에 난수를 고정합니다.
     set_seed(training_args.seed)
 
     datasets = load_from_disk(data_args.dataset_name)
     print(datasets)
 
-    # AutoConfig를 이용하여 pretrained model 과 tokenizer를 불러옵니다.
-    # argument로 원하는 모델 이름을 설정하면 옵션을 바꿀 수 있습니다.
     config = AutoConfig.from_pretrained(
-        model_args.config_name
-        if model_args.config_name is not None
-        else model_args.model_name_or_path,
+        model_args.config_name if model_args.config_name is not None else model_args.model_name_or_path,
     )
+
     tokenizer = AutoTokenizer.from_pretrained(
-        model_args.tokenizer_name
-        if model_args.tokenizer_name is not None
-        else model_args.model_name_or_path,
-        # 'use_fast' argument를 True로 설정할 경우 rust로 구현된 tokenizer를 사용할 수 있습니다.
-        # False로 설정할 경우 python으로 구현된 tokenizer를 사용할 수 있으며,
-        # rust version이 비교적 속도가 빠릅니다.
+        model_args.tokenizer_name if model_args.tokenizer_name is not None else model_args.model_name_or_path,
         use_fast=True,
     )
+
+    # QA task
     model = AutoModelForQuestionAnswering.from_pretrained(
         model_args.model_name_or_path,
         from_tf=bool(".ckpt" in model_args.model_name_or_path),
@@ -97,10 +95,36 @@ def main():
         type(model),
     )
 
-    # do_train mrc model 혹은 do_eval mrc model
-    if training_args.do_train or training_args.do_eval:
+    # keep train params 추가
+    if training_args.do_train:
+        training_args.load_best_model_at_end = True
+        training_args.metric_for_best_model = 'loss'
+
+        training_args.logging_dir = './logs'
+        training_args.logging_steps = 300
+        training_args.save_steps = 300
+        training_args.save_total_limit = 10
+        training_args.gradient_checkpointing = True
+        training_args.per_device_train_batch_size = 8
+
+        training_args.reported_to = 'wandb'
+
+    # keep eval params 추가
+    if training_args.do_eval:
+        training_args.evaluation_strategy = 'steps'
+        # training_args.eval_steps = 100,
+        training_args.per_device_eval_batch_size = 8
+        # training_args.eval_accumulation_steps = 3 # keep 메모리 부족 시
+
+    # if training_args.do_train or training_args.do_eval:
+    if training_args.do_train and training_args.do_eval:
         run_mrc(data_args, training_args, model_args, datasets, tokenizer, model)
 
+
+metric = load_metric("squad")
+def custom_metrics(p: EvalPrediction):
+    score = metric.compute(predictions=p.predictions, references=p.label_ids)
+    return score
 
 def run_mrc(
     data_args: DataTrainingArguments,
@@ -110,6 +134,7 @@ def run_mrc(
     tokenizer,
     model,
 ) -> NoReturn:
+    # exit()
 
     # dataset을 전처리합니다.
     # training과 evaluation에서 사용되는 전처리는 아주 조금 다른 형태를 가집니다.
@@ -220,7 +245,8 @@ def run_mrc(
             batched=True,
             num_proc=data_args.preprocessing_num_workers,
             remove_columns=column_names,
-            load_from_cache_file=not data_args.overwrite_cache,
+            # load_from_cache_file=not data_args.overwrite_cache,
+            load_from_cache_file=data_args.overwrite_cache,
         )
 
     # Validation preprocessing
@@ -236,7 +262,7 @@ def run_mrc(
             return_overflowing_tokens=True,
             return_offsets_mapping=True,
             #return_token_type_ids=False, # roberta모델을 사용할 경우 False, bert를 사용할 경우 True로 표기해야합니다.
-            padding="max_length" if data_args.pad_to_max_length else False,
+            padding="max_length" if data_args.pad_to_max_length else False, # keep default True
         )
 
         # 길이가 긴 context가 등장할 경우 truncate를 진행해야하므로, 해당 데이터셋을 찾을 수 있도록 mapping 가능한 값이 필요합니다.
@@ -271,7 +297,8 @@ def run_mrc(
             batched=True,
             num_proc=data_args.preprocessing_num_workers,
             remove_columns=column_names,
-            load_from_cache_file=not data_args.overwrite_cache,
+            # load_from_cache_file=not data_args.overwrite_cache,
+            load_from_cache_file=data_args.overwrite_cache,
         )
 
     # Data collator
@@ -281,7 +308,7 @@ def run_mrc(
         tokenizer, pad_to_multiple_of=8 if training_args.fp16 else None
     )
 
-    # Post-processing:
+    # keep 사후처리 =>
     def post_processing_function(examples, features, predictions, training_args):
         # Post-processing: start logits과 end logits을 original context의 정답과 match시킵니다.
         predictions = postprocess_qa_predictions(
@@ -291,7 +318,8 @@ def run_mrc(
             max_answer_length=data_args.max_answer_length,
             output_dir=training_args.output_dir,
         )
-        # Metric을 구할 수 있도록 Format을 맞춰줍니다.
+
+        # for metric 계산
         formatted_predictions = [
             {"id": k, "prediction_text": v} for k, v in predictions.items()
         ]
@@ -307,25 +335,35 @@ def run_mrc(
                 predictions=formatted_predictions, label_ids=references
             )
 
-    metric = load_metric("squad")
+    print()
+    print('--- Check Eval Data---')
+    print(eval_dataset)
+    print()
 
-    def compute_metrics(p: EvalPrediction):
-        return metric.compute(predictions=p.predictions, references=p.label_ids)
+    print()
+    print('--- Check Training Args ---')
+    print(training_args)
+    print()
 
-    # Trainer 초기화
+    # custom trainer
     trainer = QuestionAnsweringTrainer( 
         model=model,
         args=training_args,
-        train_dataset=train_dataset if training_args.do_train else None,
-        eval_dataset=eval_dataset if training_args.do_eval else None,
+        # train_dataset=train_dataset if training_args.do_train else None,
+        train_dataset=train_dataset,
+        # eval_dataset=eval_dataset if training_args.do_eval else None,
+        eval_dataset=eval_dataset,
         eval_examples=datasets["validation"] if training_args.do_eval else None,
         tokenizer=tokenizer,
         data_collator=data_collator,
         post_process_function=post_processing_function,
-        compute_metrics=compute_metrics,
+        compute_metrics=custom_metrics,
+        callbacks=[
+            EarlyStoppingCallback(early_stopping_patience=3)
+        ]
     )
 
-    # Training
+    # train
     if training_args.do_train:
         if last_checkpoint is not None:
             checkpoint = last_checkpoint
@@ -333,12 +371,14 @@ def run_mrc(
             checkpoint = model_args.model_name_or_path
         else:
             checkpoint = None
-        train_result = trainer.train(resume_from_checkpoint=checkpoint)
-        trainer.save_model()  # Saves the tokenizer too for easy upload
+        train_result = trainer.train(
+            # keep 모델 캐시 차단
+            # resume_from_checkpoint=checkpoint
+        )
 
+        trainer.save_model()
         metrics = train_result.metrics
         metrics["train_samples"] = len(train_dataset)
-
         trainer.log_metrics("train", metrics)
         trainer.save_metrics("train", metrics)
         trainer.save_state()
@@ -346,26 +386,26 @@ def run_mrc(
         output_train_file = os.path.join(training_args.output_dir, "train_results.txt")
 
         with open(output_train_file, "w") as writer:
-            logger.info("***** Train results *****")
+            logger.info("***** Train Results *****")
             for key, value in sorted(train_result.metrics.items()):
-                logger.info(f"  {key} = {value}")
+                logger.info(f"{key} = {value}")
                 writer.write(f"{key} = {value}\n")
 
-        # State 저장
         trainer.state.save_to_json(
             os.path.join(training_args.output_dir, "trainer_state.json")
         )
 
-    # Evaluation
+    # eval
     if training_args.do_eval:
-        logger.info("*** Evaluate ***")
+        logger.info("***** Evaluate *****")
         metrics = trainer.evaluate()
-
         metrics["eval_samples"] = len(eval_dataset)
-
         trainer.log_metrics("eval", metrics)
         trainer.save_metrics("eval", metrics)
 
+    # keep 캐시 메모리 초기화
+    del model, trainer, training_args
+    torch.cuda.empty_cache()
 
 if __name__ == "__main__":
     main()
